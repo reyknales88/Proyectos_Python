4a06a99c98e16372785965b2650e6d3734a99d27
reyknales88 <reyknales88@gmail.com>
2025-05-19 17:06:59 -0600

    arcchivos de la carpeta funciones

---
+++ FUNCIONES/Paso de parametros posicionales.py
@@ -0,0 +1,14 @@
+def intro(primer_nombre, apellido):
+    print("Hola, mi nombre es", primer_nombre, apellido)
+
+intro("Luke", "Skywalker")
+intro("Jesse", "Quick")
+intro("Clark", "Kent")
+
+def hungria(primer_nombre, apellido):
+    print("Hola, mi nombre es", primer_nombre, apellido)
+
+hungria("Skywalker", "Luke")
+hungria("Quick", "Jesse")
+hungria("Kent", "Clark")
+
---
+++ FUNCIONES/ejemplo.py
@@ -0,0 +1,6 @@
+def mensaje(hey, num):
+    print("Ingrese un nombre: ", hey, "numero: ", num)
+    
+num = 4
+mensaje("Carlos",1)
+print(num)
